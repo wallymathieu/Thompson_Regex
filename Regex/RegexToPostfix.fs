0840e51fa77c47dff3e45963adb92c3e9c45c380
Oskar Gewalli <oskar@gewalli.se>
2015-10-14 22:41:12 +0200

    Just some spaces etc

--- Regex/RegexToPostfix.fs
+++ Regex/RegexToPostfix.fs
@@ -17,19 +17,19 @@ let rec re2post'
             failwith "unmatched!"
         else
             (List.replicate (s.natom-1) '.' ) @ (List.replicate s.nalt '|')
-    | c:: t ->
+    | c:: tail ->
         match c with
         | '('->
             if(s.natom > 1) then
-                '.' :: (re2post' paren {s with natom=s.natom-1} t)
+                '.' :: (re2post' paren {s with natom=s.natom-1} tail)
             else
-                (re2post' (s:: paren) {natom=0; nalt=0} t)
+                (re2post' (s:: paren) {natom=0; nalt=0} tail)
         | '|'->
             if(s.natom = 0) then
                 failwith "unmatched!";
             else
                 let dots = List.replicate (s.natom-1) '.' 
-                dots @ (re2post' paren {nalt=s.nalt+1; natom=0} t)
+                dots @ (re2post' paren {nalt=s.nalt+1; natom=0} tail)
         | ')'->
             if(s.natom = 0) then
                 failwith "unmatched!";
@@ -37,7 +37,7 @@ let rec re2post'
                 let ops = (List.replicate (s.natom-1) '.') @ (List.replicate s.nalt '|' )
                 match paren with
                 | p::paren_rest ->
-                    ops @ (re2post' paren_rest {nalt=p.nalt; natom=p.natom+1} t)
+                    ops @ (re2post' paren_rest {nalt=p.nalt; natom=p.natom+1} tail)
                 | _ -> failwith "unmatched!"
         | '*'
         | '+'
@@ -45,8 +45,8 @@ let rec re2post'
             if(s.natom = 0) then
                 failwith "unmatched!"
             else
-                c::(re2post' paren s t)
-        | c' ->
+                c::(re2post' paren s tail)
+        | _ ->
             let mutable natom = s.natom
             let op' =
                 if(natom > 1) then
@@ -55,8 +55,8 @@ let rec re2post'
                 else
                     []
             natom<-natom+1
-            op' @ [c'] @ (re2post' paren {s with natom=natom} t)
+            op' @ [ c ] @ (re2post' paren {s with natom=natom} tail)
 
 let re2post(re:string)=
-    let result = re2post' [] {natom=0;nalt=0} ( re.ToCharArray() |> Array.toList )
+    let result = re2post' [] { natom=0; nalt=0 } ( re.ToCharArray() |> Array.toList )
     new String(result |> List.toArray)
