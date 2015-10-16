d24584aedaec4e03a04895d36ec77162ea0554ce
Oskar Gewalli <oskar@gewalli.se>
2015-10-17 01:37:54 +0200

    isMatch should return boolean

--- Regex/Regex.fs
+++ Regex/Regex.fs
@@ -6,6 +6,18 @@ open RegexToPostfix
 type MList<'t> when 't : equality=
     System.Collections.Generic.List<State<'t>>
 
+module MList=
+    let add v (l:MList<'t>)=
+        l.Add(v)
+
+    let tryFind predicate (l:MList<'t>)=
+         let v = l.FindIndex ( fun e->predicate e)
+         if v<0 then
+            None
+         else
+            Some(l.[v])
+
+
 type ListState(listid:int) =
     member val listid=listid with get, set
     member val lastList = new System.Collections.Generic.Dictionary<int,int>()
@@ -14,9 +26,7 @@ type ListState(listid:int) =
     new ()=
         new ListState(0)
     static member getLastList v (ls:ListState) =
-        if ls.lastList.ContainsKey(v) then
-            ()
-        else
+        if not( ls.lastList.ContainsKey(v) ) then
             ls.lastList.Add(v,0)
         ls.lastList.[v]
 
@@ -27,17 +37,17 @@ type ListState(listid:int) =
         ls.lastList.[v] <- ls.listid
 
 /// Run NFA to determine whether it matches s.
-let matches (start:State<'t>) (s:'t seq) :bool=
+let matches (start:State<'t>) (s:'t seq) (visit:State<'t>->'a) :'a seq option =
     /// Check whether state list contains a match.
     let ismatch (l:MList<'t>)=
-        l.FindIndex( fun s-> NFAState<'t>.isMatch s.c ) >= 0 
+        l 
+            |> MList.tryFind ( fun s-> NFAState<'t>.isMatch s.c )
+            |> Option.isSome
 
     /// Add s to l, following unlabeled arrows.
     let rec addstate (ls:ListState) (l:MList<'t>) (s:State<'t>)=
         // (s == NULL || s->lastlist == listid)
-        if (ListState.isListid s.id ls)  then 
-            ()
-        else
+        if not(ListState.isListid s.id ls)  then 
             // s->lastlist = listid;
             ListState.setListid s.id ls
 
@@ -46,11 +56,9 @@ let matches (start:State<'t>) (s:'t seq) :bool=
                 let addstate = Option.iter(addstate ls l) 
                 addstate s.out
                 addstate s.out1
-                // return;
             else
-                // l->s[l->n++] = s;
-                l.Add(s)
-            ()
+                l |> MList.add s
+
 
     /// Step the NFA from the states in clist
     /// past the character c,
@@ -58,10 +66,13 @@ let matches (start:State<'t>) (s:'t seq) :bool=
     let step (ls:ListState) (clist:MList<'t>, c:'t, nlist:MList<'t>)=
         ls.listid_plus_plus()
         nlist.Clear()
-        for s in clist do
-            if s.c.matches c then
-                let addstate = Option.iter(addstate ls nlist) 
-                addstate s.out
+        let maybeS = clist |> MList.tryFind (fun s->s.c.matches c)
+        maybeS |> Option.map (fun s->
+            let h = visit s
+            let optAddstate = Option.iter(addstate ls nlist) 
+            optAddstate s.out
+            h
+        )
 
     /// Compute initial state list
     let startlist (ls:ListState) (start:State<'t>,l:MList<'t>)=
@@ -75,17 +86,21 @@ let matches (start:State<'t>) (s:'t seq) :bool=
     let l2 = new MList<_>()
     let mutable clist = startlist ls (start, l1)
     let mutable nlist = l2
+    let res = new System.Collections.Generic.List<'a option>()
     for c in s do
-        step ls (clist, c, nlist)
+        res.Add( step ls (clist, c, nlist) )
         // swap clist, nlist :
         let t = clist
         clist <- nlist
         nlist <- t
     // 
 
-    ismatch clist
+    if ismatch clist then
+        Some (res |> Seq.choose id)
+    else
+        None
 
-let isMatch (regex, value)=
+let isMatch (regex, value) = 
     let post2format (p:string)=
         let to_m c=
             match OperationType.fromChar c with
@@ -100,5 +115,6 @@ let isMatch (regex, value)=
     re2post(regex)
         |> post2format
         |> NFA.post2nfa
-        |> Option.map (fun start-> matches start value)
+        |> Option.bind (fun start-> matches start value id)
+        |> Option.isSome
     
--- Tests/RegexTests.fs
+++ Tests/RegexTests.fs
@@ -3,16 +3,14 @@ open NUnit.Framework
 
 [<TestFixture>]
 type RegexTests() = 
-    member self._true=Is.EqualTo(Some true)
-
     [<Test>]
     member self.A_or_b()=
-        Assert.That(Regex.isMatch ("(a|b)", "a"), self._true)
+        Assert.That(Regex.isMatch ("(a|b)", "a"), Is.True)
 
     [<Test>]
     member self.A_followed_by_b()=
-        Assert.That(Regex.isMatch ("ab", "ab"), self._true)
+        Assert.That(Regex.isMatch ("ab", "ab"), Is.True)
 
     [<Test>]
     member self.Just_A()=
-        Assert.That(Regex.isMatch ("a", "a"), self._true)
+        Assert.That(Regex.isMatch ("a", "a"), Is.True)
